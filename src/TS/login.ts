fb6d0c21fdf4e960f91c8f99480ff937fa6ad1f8
korbandras <korbandras@gmail.com>
2022-11-02 13:13:05 +0100

    before webprog

--- src/TS/login.ts
+++ src/TS/login.ts
@@ -1,29 +1,48 @@
 import {Component} from "@angular/core";
 import {FormControl} from "@angular/forms";
-import {Observable} from "rxjs";
+import {Observable, of} from "rxjs";
 import {User} from "../Interfaces/User";
-import {HttpClient} from "@angular/common/http";
+import {HttpClient, HttpResponse} from "@angular/common/http";
 
 @Component({
   selector: 'app-root',
   templateUrl: '../Pages/Login.html',
   styleUrls: ['../CSS/loginStyle.css']
 })
-export class NewEditorComponent{
+export class AuthenticationService{
   username = new FormControl('');
   users: Observable<User[]> | undefined;
   readonly ROOT_Url = 'http://127.0.0.1:8080/Ugyfel_Gate_Rest_war_exploded/rest/get'
-  constructor(private http:HttpClient) {}
+  constructor(private http:HttpClient) { }
 
   onSubmit() {
     //const users = this.http.get<User[]>(this.ROOT_Url+'/users');
     let users: Observable<User[]>[] = new Array(this.http.get<User[]>(this.ROOT_Url + '/users'));
+    console.log(users);
+  }
+
+  logIn(username: HTMLInputElement): Observable<any>{
+    let users: Observable<User[]>[] = new Array(this.http.get<User[]>(this.ROOT_Url + '/users'));
+    const usersName = document.getElementById("username");
     for(let i = 0; i < users.length; i++){
-      if (users[i] === this.username) {
-        console.log("yes");
-      } else {
-        console.log("no");
+      if(usersName == users[i]){
+        localStorage.setItem("token", "serverToken");
+        return of(new HttpResponse({status: 200}));
+      }
+      else{
+        return of(new HttpResponse({status: 401}));
       }
     }
+  }
+
+  logOut(): void{
+    localStorage.removeItem("token");
+  }
+
+  isLoggedIn(): boolean{
+    if(localStorage.getItem("token") != null){
+      return true;
+    }
+    return false;
   }
 }
